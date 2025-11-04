2ac28d41094dc5101c3408c18451c1cd42dd9c53
Alex Neal <afneal0226@gmail.com>
2025-11-04 13:05:55 -0600

    fixed typo in text

--- src/components/Pages/TravelTips.jsx
+++ src/components/Pages/TravelTips.jsx
@@ -13,9 +13,9 @@ function TravelTips () {
                         <li>Make copies of important documents</li>
                         <li>Alert credit card companies and banks of travel dates</li>
                         <li>Bring additional currency for emergency purchases</li>
-                        <li>Consider an international phone plan or SIM card if travelling abroad</li>
+                        <li>Consider an international phone plan or SIM card if traveling abroad</li>
                         <li>Pack light</li>
-                        <li>Get all required international vaccinations if travelling abroad</li>
+                        <li>Get all required international vaccinations if traveling abroad</li>
                     </ul>
                 </p>
             </div>
