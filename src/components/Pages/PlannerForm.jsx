33906ce24f22caa6574963e324f37c5e7e447442
Alex Neal <afneal0226@gmail.com>
2025-11-06 13:54:11 -0600

    added to planner form

--- src/components/Pages/PlannerForm.jsx
+++ src/components/Pages/PlannerForm.jsx
@@ -1,40 +1,8 @@
 import { useState } from 'react'
 
 function PlannerForm() {
-    const [formData, setFormData] = useState({
-        city: "",
-        date: "",
-    })
-    const handleInput = e => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        })
-    }
 
-    const [activities, setActivities] = useState([{
-        activity: "",
-        time: "",
-        notes: "",
-    }])
-
-    const addActivity = (e, i) => {
-        const { name, value } = e.target; 
-        let newActivity = [...activities];
-        newActivity[i][name] = value;
-        setActivities(newActivity);
-    }
-
-    const handleAddActivity = () => {
-        setActivities([...activities, {
-            activity: "",
-            time: "",
-            notes: "",
-        }])
-    }
-
-    const [days, setDays] = useState([{
+    const [days, setDays] = useState([{ //initialize days array with useState hook to empty strings in the days objects
         city: "",
         date: "",
         activities: [
@@ -43,8 +11,23 @@ function PlannerForm() {
 
     }])
 
+
+
+    const handleAddActivity = (dayIndex) => {
+        const newDays = [...days];  //make a copy of the "days" array without mutating original
+        newDays[dayIndex].activities.push({  //adds a blank activity object to the specific day index
+            activity: "",
+            time: "",
+            notes: "",
+        });
+        setDays(newDays);  //update reacts state to let it know that data was updated to re-render that portion
+    }
+
+
+
+
     const handleAddDay = () => {
-        setDays([...days, {
+        setDays([...days, {  //updates state with new empty day to the array
             city: "",
             date: "",
             activities: [
@@ -53,118 +36,132 @@ function PlannerForm() {
         }])
     }
 
-    const handleDelete = (i) => {
-        let deleteActivty = [...activities];
-        deleteActivty.splice(i, 1);
-        setActivities(deleteActivty);
+
+    const handleActivityDelete = (dayIndex, activityIndex) => {
+        const newDays = [...days];  //makes copy of days array
+        newDays[dayIndex].activities.splice(activityIndex, 1); //splices the activity from the specific day index,deletes 1 element
+        setDays(newDays);  //updates the state
+    }
+
+
+    const handleDayDelete = (dayIndex) => {
+        const newDays = [...days];
+        newDays.splice(dayIndex, 1); //delete the whole day array at the index (not just the day object)
+        setDays(newDays);
     }
 
 
     return (
         <>
             <h1 className='trip-details'>Trip Details</h1>
-            <form className='form'>
-                {days.map((day, dayIndex) => (
-                <fieldset key={dayIndex}>
-                    <legend>Day and Location</legend>
-                    
-                    <label>What is the city?</label>
-                    <input
-                        id="city"
-                        name="city"
-                        placeholder='City Name'
-                        onChange={e => {
-                            const newDays = [...days];
-                            newDays[dayIndex].city = e.target.value;
-                            setDays(newDays);
-                        }}
+            <form className='form'> 
+                {days.map((day, dayIndex) => ( //map in react needs a unique key to match elements when updating the virtual DOM. 
+                                                //map through the days array to make a new form fieldset for each new day
+                    <fieldset key={dayIndex}>
+                        <legend>Day and Location</legend>
 
-                    />
-                    <label>What is the date of activities?</label>
-                    <input
-                        id="date"
-                        name="date"
-                        type='date'
-                        placeholder='Date of Activities'
-                        onChange={e => {
-                            const newDays = [...days];
-                            newDays[dayIndex].date = e.target.value;
-                            setDays(newDays);
-                        }}
-                    />
-                    
-                
+                        <label>What is the city?</label>
+                        <input
+                            id="city"
+                            name="city"
+                            placeholder='City Name'
+                            onChange={e => { //updates when user types in input field
+                                const newDays = [...days]; //create array copy without mutating original
+                                newDays[dayIndex].city = e.target.value; //sets the city input at the specific day index to the value the user is typing
+                                setDays(newDays);
+                            }}
 
-                    <legend>Activities</legend>
+                        />
+                        <label>What is the date of activities?</label>
+                        <input
+                            id="date"
+                            name="date"
+                            type='date'
+                            placeholder='Date of Activities'
+                            onChange={e => {
+                                const newDays = [...days];
+                                newDays[dayIndex].date = e.target.value;
+                                setDays(newDays);
+                            }}
+                        />
 
-                    {day.activities.map((act, index) => (
-                        <div key={index} className='activity-component'>
-                            <div className='activity-input'>
-                                <label>Input Activity</label>
 
-                                <input
-                                    id="activity"
-                                    name="activity"
-                                    placeholder='Activity'
-                                    onChange={e => {
-                                        const newActivities = [...activities];
-                                        newActivities[index].activity = e.target.value;
-                                        setActivities(newActivities)
-                                    }}
-                                />
+
+                        <legend>Activities</legend>
+
+                        {day.activities.map((activity, activityIndex) => (
+                            <div key={activityIndex} className='activity-component'>
+                                <div className='activity-input'>
+                                    <label>Input Activity</label>
+
+                                    <input
+                                        id="activity"
+                                        name="activity"
+                                        placeholder='Activity'
+                                        onChange={e => {
+                                            const newDays = [...days];
+                                            newDays[dayIndex].activities[activityIndex].activity = e.target.value;
+                                            setDays(newDays);
+                                        }}
+                                    />
+
+                                </div>
+                                <div className='time-input'>
+                                    <label>Time of Activity</label>
+                                    <input
+                                        id="time"
+                                        name="time"
+                                        placeholder='Time of Activity'
+                                        onChange={e => {
+                                            const newDays = [...days];
+                                            newDays[dayIndex].activities[activityIndex].time = e.target.value;
+                                            setDays(newDays);
+                                        }}
+                                    />
+                                </div>
+                                <div className='activity-notes'>
+                                    <label>Notes</label>
+                                    <textarea
+                                        id="notes"
+                                        name="notes"
+                                        rows={5}
+                                        placeholder='Add Notes'
+                                        onChange={e => {
+                                            const newDays = [...days];
+                                            newDays[dayIndex].activities[activityIndex].notes = e.target.value;
+                                            setDays(newDays);
+                                        }}
+                                    />
+                                </div>
+
+                                <button
+                                    className='delete-activity-button'
+                                    type="button" //Parameters: Delete the specific activity index at the specific day index
+                                    onClick={() => handleActivityDelete(dayIndex, activityIndex)}>Delete Activity</button>
+                                    
 
                             </div>
-                            <div className='time-input'>
-                                <label>Time of Activity</label>
-                                <input
-                                    id="time"
-                                    name="time"
-                                    placeholder='Time of Activity'
-                                    onChange={e => {
-                                        const newActivities = [...activities];
-                                        newActivities[index].time = e.target.value;
-                                        setActivities(newActivities);
-                                    }}
-                                />
-                            </div>
-                            <div className='activity-notes'>
-                                <label>Notes</label>
-                                <textarea
-                                    id="notes"
-                                    name="notes"
-                                    rows={5}
-                                    placeholder='Add Notes'
-                                    onChange={e => {
-                                        const newActivities = [...activities];
-                                        newActivities[index].notes = e.target.value;
-                                        setActivities(newActivities);
-                                    }}
-                                />
-                            </div>
-                            
-                            <button
-                                className='delete-button'
-                                type="button"
-                                onClick={() => handleDelete(index)}>Delete</button>
+                        ))}
+                        <div>
+
+                            <button className='activity-button'
+                                type="button" //Add activity at the specific dayIndex listed as a parameter
+                                onClick={() => handleAddActivity(dayIndex)}>Add Activity</button>
 
                         </div>
-                    ))}
-                    <div>
 
-                        <button className='activity-button'
-                            type="button"
-                            onClick={handleAddActivity}>Add Activity</button>
+                        <div>
+                            <button className='delete-day-button'
+                                type="button"
+                                onClick={() => handleDayDelete(dayIndex)}>Delete Day</button>
+                        </div>
 
-                        
-
-                    </div>
-                   
-                </fieldset>
+                    </fieldset>
                 ))}
                 <button className='add-day-button'
-                    type="button"
+                    type="button" //No parameters so no need for arrow function to call function
                     onClick={handleAddDay}>Add Day</button>
-                
+
 
             </form>
 
--- src/components/form/Activities.jsx
+++ src/components/form/Activities.jsx
@@ -2,7 +2,17 @@ import { useState } from 'react';
 
 function Activities() {
     const [isEditingActivities, setIsEditingActivities] = useState(false);
-    const [activities, setActivities] = useState('Click to Add Activities');
+    const [activities, setActivities] = useState(['Click to Add Activities']); //activites must be array to use map later
+
+    const handleAddActivity = (index, value) => {  //set index and value parameters
+        const updatedActivities = [...activities];  //make a copy of all activities
+        updatedActivities[index] = value;       //updates the activity at the index to the new value activity
+        setActivities(updatedActivities); //update state with added activities
+    }
+
+    const addActivity = () => {
+        setActivities([...activities, '']); //button creates a new array of activities with an empty string at the end
+    }
 
     return (
         <>
@@ -10,21 +20,40 @@ function Activities() {
                 <div>
                     {isEditingActivities ? (
                         <div>
-                            <input
-                                type="text"
-                                value={activities}
-                                onChange={(e) => setActivities(e.target.value)}
-                                className='activities-input'
-                            />
+                            {activities.map((activity, index) => (  //loop through activities at each index
+
+                                <div key={index} className='activity-item'>
+                                    <input
+                                        type="text"
+                                        value={activity}
+                                        onChange={(e) => handleAddActivity(index, e.target.value)} //update the index with new value
+                                    />
+                                </div>
+                            ))}
+
                             <button onClick={(e) => setIsEditingActivities(false)}>Save</button>
                         </div>
+
                     ) : (
-                        <div onClick={() => setIsEditingActivities(true)}>
+
+                        <div>
+
+
                             <span className='activites-text'>{activities}</span>
 
 
                         </div>
                     )}
+                    <div>
+                        <button className='edit-button'
+                            type="button"
+                            onClick={() => setIsEditingActivities(true)}>Edit</button>
+
+                        <button className='add-button'
+                            type="button"
+                            onClick={ addActivity }>Add Activity</button>
+                    </div>
+
                 </div>
             </div >
         </>
@@ -32,3 +61,4 @@ function Activities() {
 }
 
 export default Activities;
+
