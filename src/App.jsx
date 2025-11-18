27cc53ee400cf046bf5f5cc85b787c670d235c5e
Alex Neal <afneal0226@gmail.com>
2025-11-18 12:29:20 -0600

    added hamburger menu and styling

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import TravelTips from './components/Pages/TravelTips'
 import BuildTrips from './components/Pages/BuildTrip';
 import FlightInfo from './components/planner/FlightInfo';
 import List from './components/PlannerComponents/List';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   
@@ -19,10 +20,8 @@ const [trips, setTrips] = useState([]);
 const [packingList, setPackingList] = useState([])
 const [list, setList] = useState([])
 const [flightData, setFlightData] = useState([]);
-const [connFlightData, setConnFlightData] = useState([]);
-const [packingListData, setPackingListData] = useState([]);
 const [reminderList, setReminderList] = useState([])
-const [reminderListData, setReminderListData] = useState([])
+
 
 
 
@@ -31,20 +30,18 @@ const [reminderListData, setReminderListData] = useState([])
     {/* <div className='App' style={{backgroundImage: `url(${travelBackground})`, backgroundSize: 'cover', backgroundPosition:'center', minHeight:'100vh',
   margin: '0', padding: '0', opacity: 0.9}}> */}
       <Header />
-      
+      <ToastContainer position="top-right" autoClose={1000} />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/traveltips" element={<TravelTips />} />
         <Route path="/planner" element={<Planner />} />
         <Route path="/buildtrips" element={<BuildTrips trips={trips} setTrips={setTrips} flightData={flightData} setFlightData={setFlightData}
-         connFlightData={connFlightData} setConnFlightData={setConnFlightData} packingList={packingList} setPackingList={setPackingList} 
-         packingListData={packingListData} setPackingListData={setPackingListData} list={list} setList={setList} reminderList={reminderList} setReminderList={setReminderList}
-         reminderListData={reminderListData} setReminderListData={setReminderListData}/>} />
+         packingList={packingList} setPackingList={setPackingList} list={list} setList={setList} reminderList={reminderList} setReminderList={setReminderList}
+         />} />
 
-        <Route path="/savedtrips" element={<SavedTrips trips={trips} setTrips={setTrips} connFlightData={connFlightData} setConnFlightData={setConnFlightData} 
-        packingList={packingList} setPackingList={setPackingList} packingListData={packingListData} setPackingListData={setPackingListData}
+        <Route path="/savedtrips" element={<SavedTrips trips={trips} setTrips={setTrips} packingList={packingList} setPackingList={setPackingList} 
         list={list} setList={setList} flightData={flightData} setFlightData={setFlightData} reminderList={reminderList} setReminderList={setReminderList}
-         reminderListData={reminderListData} setReminderListData={setReminderListData}/>} />
+         />} />
         
         
         {/* <Route path="/resources" element={<Resources />} /> */}
--- src/components/planner/Reminders.jsx
+++ src/components/planner/Reminders.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import List from "../PlannerComponents/List";
 import SubmitButton from "../PlannerComponents/SubmitButton";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ReminderList({ reminderList, setReminderList, reminderListData, setReminderListData }) {
     const [list, setList] = useState([])
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setReminderList([...reminderList, ...list]);
-        setReminderListData([...reminderListData, list]);
-        alert("Reminder list successfully saved");
+        setReminderList([...reminderList, list]);
+        // setReminderList([...reminderList, ...list]);
+        // setReminderListData([...reminderListData, list]);
         setList([])
+        toast.success("Reminders List Successfully Saved!");
     }
     return (
         <>
